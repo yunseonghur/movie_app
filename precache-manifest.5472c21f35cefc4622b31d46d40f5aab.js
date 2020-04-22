ff53995cd46c280450d1422076e599ab59432dd3
Yuni <yshuh0126@gmail.com>
2020-04-21 23:41:43 -0700

    Updates

---
+++ precache-manifest.5472c21f35cefc4622b31d46d40f5aab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cc1b7098013d07b3d2277ae178ad519",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "c183bfa211c03570dcda",
+    "url": "/movie_app/static/css/main.05d37451.chunk.css"
+  },
+  {
+    "revision": "98be54eaa9ed6a5a4f95",
+    "url": "/movie_app/static/js/2.cfee456d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.cfee456d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c183bfa211c03570dcda",
+    "url": "/movie_app/static/js/main.96b63db8.chunk.js"
+  },
+  {
+    "revision": "4dcb005dec2f66fe5231",
+    "url": "/movie_app/static/js/runtime-main.615d8c0e.js"
+  }
+]);
